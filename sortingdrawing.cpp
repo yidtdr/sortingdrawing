6df63b6d90c28d781a171f2b8bcd7f6cc162a56c
yidtdr <144206744+yidtdr@users.noreply.github.com>
2023-10-26 00:25:58 +0300

    Добавьте файлы проекта.

---
+++ sortingdrawing.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(), "ArraySorting", sf::Style::Fullscreen);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
